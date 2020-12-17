2544f0f620dc56add500d815f1aac1b8294c1a08
Baileylin <baileylinhfi@gmail.com>
2020-12-17 02:30:29 -0500

    working on optional(encountered crashing issue)

--- app/src/main/java/com/example/simpletodo/ItemsAdpater.java
+++ app/src/main/java/com/example/simpletodo/ItemsAdpater.java
@@ -11,14 +11,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class ItemsAdpater extends RecyclerView.Adapter<ItemsAdpater.Viewholder>{
+    public interface OnClickListener{
+        void onItemClicked(int position);
+    }
     public interface OnLongClickListener{
         void onItemLongClicked(int position);
     }
+
     List<String> items;
     OnLongClickListener longClickListener;
-    public ItemsAdpater(List<String> items, OnLongClickListener longClickListener) {
+    OnClickListener clickListener;
+    public ItemsAdpater(List<String> items, OnLongClickListener longClickListener, OnClickListener clickListener) {
         this.items=items;
         this.longClickListener=longClickListener;
+        this.clickListener=clickListener;
     }
 
     @NonNull
@@ -49,6 +55,12 @@ public class ItemsAdpater extends RecyclerView.Adapter<ItemsAdpater.Viewholder>{
 
         public void bind(String item) {
             tvItem.setText(item);
+            tvItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clickListener.onItemClicked(getAdapterPosition());
+                }
+            });
             tvItem.setOnLongClickListener(new View.OnLongClickListener() {
                 @Override
                 public boolean onLongClick(View v) {
--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,7 +21,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String KEY_ITEM_TEXT="item_text";
+    public static final String KEY_ITEM_POSITION="item_position";
+    public static final int EDIT_TEXT_CODE=20;
     List<String> items;
     Button btnAdd;
     EditText etItem;
@@ -43,7 +46,17 @@ public class MainActivity extends AppCompatActivity {
                 saveItems();
             }
         };
-        itemsAdpater=new ItemsAdpater(items,onLongClickListener);
+        ItemsAdpater.OnClickListener onClickListener=new ItemsAdpater.OnClickListener() {
+            @Override
+            public void onItemClicked(int position) {
+                Log.d("MainActivity","Single click at position "+position);
+                Intent i=new Intent(MainActivity.this,EditActivity.class);
+                i.putExtra(KEY_ITEM_TEXT,items.get(position));
+                i.putExtra(KEY_ITEM_POSITION,position);
+                startActivityForResult(i,EDIT_TEXT_CODE);
+            }
+        };
+        itemsAdpater=new ItemsAdpater(items,onLongClickListener,onClickListener);
         rvItems.setAdapter(itemsAdpater);
         rvItems.setLayoutManager(new LinearLayoutManager(this));
         btnAdd.setOnClickListener(new View.OnClickListener() {
